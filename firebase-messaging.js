3bf115b7b30299fa12ce2a5ba88a2095716a4c48
Xav123j <143720682+Xav123j@users.noreply.github.com>
2024-11-27 14:34:59 +0000

    Update firebase-messaging.js

--- firebase-messaging.js
+++ firebase-messaging.js
@@ -35,15 +35,13 @@ Notification.requestPermission()
     }
   });
 
-// Register the service worker
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/firebase-messaging-sw.js")
-    .then((registration) => {
-      console.log("Service Worker registered successfully:", registration);
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/tv-screen-detector/firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
     })
-    .catch((error) => {
-      console.error("Service Worker registration failed:", error);
+    .catch(function(error) {
+      console.error('Service Worker registration failed:', error);
     });
 }
 
