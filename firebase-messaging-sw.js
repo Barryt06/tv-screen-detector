e46654f90591e4480d80fc29e6ee24470787411e
bthomas895 <bthomas@pubsvs.ie>
2024-12-06 22:55:55 +0000

    Test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,9 @@
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js"
+);
 
 // Firebase configuration
 const firebaseConfig = {
@@ -15,19 +19,44 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
+// Set up the Google provider
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// Optional: Specify additional OAuth 2.0 scopes
+provider.addScope("https://www.googleapis.com/auth/firebase.messaging");
+
+// Sign in with a pop-up window
+firebase
+  .auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    // User signed in successfully
+    const user = result.user;
+    // Access the Google OAuth token if needed
+    const credential = result.credential;
+    const token = credential.accessToken;
+    // Proceed with FCM subscription
+  })
+  .catch((error) => {
+    // Handle errors here
+    console.error(error);
+  });
+
 messaging.onBackgroundMessage((payload) => {
   console.log("Background message received:", payload);
-  
-  self.clients.matchAll({ includeUncontrolled: true, type: "window" }).then((clients) => {
-    console.log("Clients")
-    clients.forEach((client) => {
-      console.log(client)
-      client.postMessage({
-        type: "FIREBASE_DATA",
-        data: payload,
+
+  self.clients
+    .matchAll({ includeUncontrolled: true, type: "window" })
+    .then((clients) => {
+      console.log("Clients");
+      clients.forEach((client) => {
+        console.log(client);
+        client.postMessage({
+          type: "FIREBASE_DATA",
+          data: payload,
+        });
       });
     });
-  });
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
